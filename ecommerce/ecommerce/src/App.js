089a3245fef3d75962036a0bc687f513c857179a
rafael-arrieta <102436201+rafael-arrieta@users.noreply.github.com>
2022-07-06 20:38:49 -0300

    6 de julio

--- ecommerce/ecommerce/src/App.js
+++ ecommerce/ecommerce/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      
       
     </div>
   );
--- ecommerce/ecommerce/src/components/NavBar.jsx
+++ ecommerce/ecommerce/src/components/NavBar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='barrita'>
+        <div className='logo'>
+            <h2>NavBar</h2>
+        </div>
+        <div className="navMenu">
+            <div className="buscar">
+                <input type='text' placeholder='Que buscás?'/>
+                <button className='btn'></button>
+            </div>
+            <a>Contacto</a>
+            <button className="carrito"> 
+                
+            </button>
+            <button className='btnSing'>Sing in</button>
+            
+        </div>
+
+
+    </div>
   )
 }
 
